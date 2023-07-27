e8c31f733ae743c2d8f16d73b81c7af338bf70ee
gulevskikh <gulevskih@mail.ru>
2023-07-27 11:46:52 +0300

    added new locators

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,5 @@
 from pages.base_page import BasePage
+from pages.locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
@@ -8,13 +9,13 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert True
+        assert "accounts/login" in self.browser.current_url, "Login link is not presented"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_USERNAME), "Login username is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_PASSWORD), "Login password is not presented"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(*LoginPageLocators.REG_EMAIL), "Registration e-mail is not presented"
+        assert self.is_element_present(*LoginPageLocators.REG_PASSWORD1), "Registration password1 is not presented"
+        assert self.is_element_present(*LoginPageLocators.REG_PASSWORD2), "Registration password2 is not presented"
