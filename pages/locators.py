55b3e8d21c566399673451e99fd26d1e544bfd22
Stanislav <gulevskih@mail.ru>
2023-08-05 22:48:08 +0300

    added new test

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,10 @@ class LoginPageLocators():
     REG_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
     REG_PASSWORD1 = (By.CSS_SELECTOR, "#id_registration-password1")
     REG_PASSWORD2 = (By.CSS_SELECTOR, "#id_registration-password2")
+
+class ProductPageLocators():
+    ADD_TO_CART = (By.CSS_SELECTOR, '[value="Добавить в корзину"]')
+    PRICE_CART = (By.XPATH, "//p[contains(text(), 'Стоимость корзины теперь составляет')]/strong")
+    PRICE_BOOK = (By.CSS_SELECTOR, ".col-sm-6.product_main > .price_color")
+    NAME_BOOK_CART = (By.XPATH, "//div[@id='messages']/div[1]/div[1]/strong")
+    NAME_BOOK = (By.CSS_SELECTOR, ".col-sm-6.product_main > h1")
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,5 @@
 from pages.base_page import BasePage
 from pages.locators import MainPageLocators
-from pages.login_page import LoginPage
 
 class MainPage(BasePage):
     def go_to_login_page(self):
